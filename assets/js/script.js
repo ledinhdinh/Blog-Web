e66e44c338eef2a12427cc5d9ccf2655571137fd
ledinhdinh <ledinhdinh2000@gmail.com>
2024-05-07 09:01:25 +0700

    [07/05/2024] - Updated - Edit file index, add style css, add script dark mode.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,18 @@ const navToogleFunc = function () {
 
 navMenuBtn.addEventListener('click', navToogleFunc);
 navCloseBtn.addEventListener('click', navToogleFunc);
+
+/** theme toogle variables. */
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+for (let i = 0; i < themeBtn.length; i++) {
+    themeBtn[i].addEventListener('click', function () {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+
+        for (let i = 0; i < themeBtn.length; i++) {
+            themeBtn[i].classList.toggle('light');
+            themeBtn[i].classList.toggle('dark');
+        }
+    })
+}
